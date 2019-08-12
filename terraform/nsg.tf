82b1090a9629f073103aab81cf1027972d8a8d81
mmpyro@gmail.com <mmpyro@gmail.com>
2019-08-12 14:59:52 +0200

    ssh rules added into nsg

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -5,8 +5,9 @@ resource "azurerm_network_security_group" "swarm_nsg" {
 
 }
 
-resource "azurerm_network_security_rule" "master_nsg_rules_ssh" {
-  name                        = "SSH"
+resource "azurerm_network_security_rule" "signle_master_nsg_rules_ssh" {
+  count                       = "${var.number_of_masters == 1 ? 1 : 0}"
+  name                        = "single_master_SSH"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
@@ -19,9 +20,23 @@ resource "azurerm_network_security_rule" "master_nsg_rules_ssh" {
   network_security_group_name = "${azurerm_network_security_group.swarm_nsg.name}"
 }
 
+resource "azurerm_network_security_rule" "nodes_nsg_rules_ssh" {
+  name                        = "nodes_SSH"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "50000-50200"
+  source_address_prefix       = "${var.ssh_allowed_source_address}"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.swarm_cluster_rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.swarm_nsg.name}"
+}
+
 resource "azurerm_network_security_rule" "master_nsg_rules_dockerAPI" {
   name                        = "DockerAPI"
-  priority                    = 101
+  priority                    = 102
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
